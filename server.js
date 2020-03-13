d3378d24d3611fe167f07ad85c3008a1229d394c
Lauren <TechnologicalType@MacBook-Air-2.local>
2020-03-12 20:50:12 -0400

    phase one - server

---
+++ server.js
@@ -0,0 +1,10 @@
+//requiring express
+const express = require('express');
+//requie path
+const path = require('path');
+
+//placing the express function in a variable 
+
+const app = express();
+
+//serving up the static files (from React)
